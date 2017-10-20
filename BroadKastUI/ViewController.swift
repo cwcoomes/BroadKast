768c1eac4def75f4645cc92aeab1bba39b410844
Deandre Hall <deandrehall00@gmail.com>
2017-10-19 23:54:44 -0500

    added files

---
+++ BroadKastUI/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  BroadKastUI
+//
+//  Created by Ubicomp4 on 10/19/17.
+//  Copyright © 2017 Ubicomp4. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func toLogin(_ sender: Any) {
+        performSegue(withIdentifier: "login2register", sender: self)
+    }
+    @IBAction func toLanding(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailField.text!, password: passwordField.text!) { (user, error) in
+            // ...
+            
+            
+        }
+        
+        performSegue(withIdentifier: "login2landing", sender: self)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BroadKastUI/registerViewController.swift
@@ -0,0 +1,47 @@
+//
+//  registerViewController.swift
+//  BroadKastUI
+//
+//  Created by Ubicomp4 on 10/19/17.
+//  Copyright © 2017 Ubicomp4. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+
+class registerViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBAction func registerButton(_ sender: Any) {
+        Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) { (user, error) in
+            // ...
+        }
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
